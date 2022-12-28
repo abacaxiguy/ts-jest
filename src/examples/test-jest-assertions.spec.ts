756f3116ef78130944bc4122a4e629417d26f962
João Lucas <Jlucasoc@hotmail.com>
2022-12-27 23:28:38 -0300

    Assertions with jest 🧪

---
+++ src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,34 @@
+describe("Primitive values", () => {
+    it("should test jest assertions", () => {
+        const number = 10;
+
+        expect(number).toBe(10);
+        expect(number).toEqual(10);
+
+        expect(number).not.toBeNull();
+        expect(number).not.toBeFalsy();
+        expect(number).toBeTruthy();
+
+        expect(number).toBeGreaterThan(9);
+        expect(number).toBeGreaterThanOrEqual(10);
+        expect(number).toBeLessThan(11);
+        expect(number).toBeLessThanOrEqual(10);
+
+        expect(number).toBeCloseTo(10.001);
+        expect(number).toBeCloseTo(9.999);
+
+        expect(number).toHaveProperty("toString");
+    });
+});
+
+describe("Objects", () => {
+    it("should test jest assertions - objects", () => {
+        const person = { name: "John", age: 22 };
+        const anotherPerson = { ...person };
+
+        expect(person).toEqual(anotherPerson);
+        expect(person).toHaveProperty("name", "John");
+        expect(person).toHaveProperty("age", 22);
+        expect(person.name).toBe("John");
+    });
+});
