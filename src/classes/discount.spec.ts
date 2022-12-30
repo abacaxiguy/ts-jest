c34de0277c07ad29cd5baa377375c50ebba67ce3
João Lucas <Jlucasoc@hotmail.com>
2022-12-29 21:31:42 -0300

    Testing Discount and Fixing Product tests 🧪

---
+++ src/classes/discount.spec.ts
@@ -0,0 +1,30 @@
+import {
+    Discount,
+    FiftyPercentDiscount,
+    NoDiscount,
+    TenPercentDiscount,
+} from "./discount";
+
+const createSut = (className: new () => Discount): Discount => {
+    return new className();
+};
+
+describe("Messenger", () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it("should have no discount", () => {
+        // System under test
+        const sut = createSut(NoDiscount);
+        expect(sut.calculate(10.99)).toBeCloseTo(10.99);
+    });
+
+    it("should apply 10% discount on price", () => {
+        const sut = createSut(TenPercentDiscount);
+        expect(sut.calculate(150.5)).toBeCloseTo(135.45);
+    });
+
+    it("should apply 50% discount on price", () => {
+        const sut = createSut(FiftyPercentDiscount);
+        expect(sut.calculate(150.5)).toBeCloseTo(75.25);
+    });
+});
